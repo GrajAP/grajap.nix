6bd90ee7f8ef0836a347b5d1205c4124064667c9
GrajAP <adampisarczyk2006@gmail.com>
2025-01-04 16:54:32 +0100

    164 current  2025-01-04 16:54:25  25.05.20250102.6df2492  6.12.2-zen1                          *

--- home/cli/nvim/lua/grajap/lazy/colors.lua
+++ home/cli/nvim/lua/grajap/lazy/colors.lua
@@ -1,20 +1,19 @@
-function ColorMyPencils(color)
-	color = color or "catppuccin-mocha"
-	vim.cmd.colorscheme(color)
+--      function ColorMyPencils(color)
+--      color = color or "catppuccin-mocha"
+--      vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-end
+--  end
 
 return {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000, config = function()
             require('catppuccin').setup({
-                disable_background = false,
+--               disable_background = false,
             })
 
-            vim.cmd("colorscheme catppuccin-mocha")
+            vim.cmd("Catppuccin Mocha")
 
-            ColorMyPencils()
         end
     }}
