50916e72b74534c5a17b509b5adef5f654a72577
GrajAP <adampisarczyk2006@gmail.com>
2025-01-07 23:36:42 +0100

    213 current  2025-01-07 23:36:38  25.05.20250104.8f3e1f8  6.12.2-zen1                          *

--- home/rice/waybar/default.nix
+++ home/rice/waybar/default.nix
@@ -149,7 +149,7 @@ in {
           format = "{}";
           tooltip = true;
           interval = 3600;
-          exec = "${wttr}";
+          exec = wttr;
           return-type = "json";
         };
         "custom/search" = {
