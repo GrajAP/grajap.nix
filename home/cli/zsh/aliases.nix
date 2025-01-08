c6900a3ed4903566b04ada14fa77c4376942cd72
GrajAP <adampisarczyk2006@gmail.com>
2025-01-08 18:47:34 +0100

    234 current  2025-01-08 18:47:30  25.05.20250104.8f3e1f8  6.12.2-zen1                          *

--- home/cli/zsh/aliases.nix
+++ home/cli/zsh/aliases.nix
@@ -10,7 +10,7 @@ with pkgs; {
     ${getExe yt-dlp} -x --continue --add-metadata --embed-thumbnail --audio-format mp3 --audio-quality 0 --metadata-from-title="%(artist)s - %(title)s" --prefer-ffmpeg -o "%(title)s.%(ext)s"'';
   cat = "${getExe bat} --style=plain";
   vpn = "mullvad";
-  nvim = "neovide";
+  #nvim = "neovide";
   uuid = "cat /proc/sys/kernel/random/uuid";
   grep = getExe ripgrep;
   wget = "wget --hsts-file=\"${config.xdg.dataHome}/wget-hsts\"";
