55ec3591c25e204819d39f8dedfec02e50bcb440
GrajAP <adampisarczyk2006@gmail.com>
2025-01-08 18:57:44 +0100

    236 current  2025-01-08 18:57:40  25.05.20250104.8f3e1f8  6.12.2-zen1                          *

--- flake.nix
+++ flake.nix
@@ -1,8 +1,17 @@
 {
   description = "GrajAPix";
+  outputs = {nixpkgs, ...} @ inputs: {
+    nixosConfigurations.grajap = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default
+        inputs.stylix.nixosModules.stylix
+      ];
+    };
+  };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nvf.url = "github:notashelf/nvf";
     stylix.url = "github:danth/stylix";
     hyprcontrib = {
       url = "github:hyprwm/contrib";
@@ -18,28 +27,5 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "github:hyprwm/Hyprland";
-  };
-
-  outputs = {
-    self,
-    nixpkgs,
-    nvf,
-    ...
-  } @ inputs: {
-    packages."x86_64-linux".default =
-      (nvf.lib.neovimConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = [./home/cli/nvf.nix];
-      })
-      .neovim;
-
-    nixosConfigurations.grajap = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./configuration.nix
-        inputs.home-manager.nixosModules.default
-        inputs.stylix.nixosModules.stylix
-      ];
-    };
   };
 }
